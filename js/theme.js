d83410348835ff4d51cebf1403e7e04e28a6dcd8
Florian Giroud <florian.giroud@gmail.com>
2017-09-13 16:37:55 +0700

    internal js

--- js/theme.js
+++ js/theme.js
@@ -21,7 +21,7 @@ $(document).ready(function() {
         var results = new RegExp("[?&]" + name + "=([^&#]*)").exec(
             window.location.href
         );
-        return (results && results[1]) || 0;
+        return results && results[1] ? results[1] : null;
     };
 
     var internal = $.urlParam("internal");
